3fb4637b4bbdcb505d7ac9754da0e5f4d7f4312b
Jason Winnebeck <gillius@gillius.org>
2013-07-28 21:24:35 -0400

    Now we can start typing some stuff. Register keydown on document and broadcast an event to prevent issues with capturing keys no matter where the "focus" is (especially since there is no focusable elements). And now we can actually type Apple but it runs off the end with TypeError for now...

--- childtyper.js
+++ childtyper.js
@@ -16,21 +16,65 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var app = angular.module( 'childtyper', [] );
+(function() {
+	"use strict";
 
-app.filter('charCodeToString', function() {
-	return function( input ) {
-		return String.fromCharCode( input );
+	var app = angular.module( 'childtyper', [] );
+
+	app.filter('charCodeToString', function() {
+		return String.fromCharCode;
+	});
+
+	function TypeTestController( $scope ) {
+		$scope.$on( 'keydown', function( e, keyEvent ) {
+			$scope.content = keyEvent.keyCode;
+			$scope.key = keyEvent.key;
+			$scope.char = keyEvent.char;
+		});
 	}
-});
+	TypeTestController.$inject = [ '$scope' ];
 
-var Typer = function( $scope ) {
-	$scope.handleKey = function( keyEvent ) {
-		$scope.content = keyEvent.keyCode;
-		$scope.key = keyEvent.key;
-		$scope.char = keyEvent.char;
+	app.controller('TypeTestController', TypeTestController);
+
+	/**
+	 *
+	 * @param word {string}
+	 */
+	function makeWordController( word ) {
+		function WordController( $scope ) {
+			$scope.word = word;
+			$scope.nextLetterIdx = 0;
+			$scope.nextLetter = word[$scope.nextLetterIdx];
+			$scope.typed = "";
+
+			$scope.$on( 'keydown', function( e, keyEvent ) {
+				if ( String.fromCharCode( keyEvent.keyCode ) === $scope.nextLetter.toUpperCase() ) {
+					$scope.typed += $scope.nextLetter;
+					$scope.nextLetterIdx++;
+					$scope.nextLetter = word[$scope.nextLetterIdx];
+				}
+			});
+		}
+		WordController.$inject = [ '$scope' ];
+
+		return WordController;
 	}
-};
-Typer.$inject = [ '$scope' ];
 
-app.controller('Typer', Typer);
+	function MainController( $scope ) {
+		$scope.wordController = makeWordController("Apple");
+	}
+	MainController.$inject = [ '$scope' ];
+
+	app.controller( 'MainController', MainController );
+
+	//Register keydown on document. On some browsers the "body" takes focus but might not take up
+	//the entire window (like on IE), so if you click outside of any content, body won't receive
+	//events anymore. So we can't use ngKeydown directive in the HTML.
+	app.run( [ '$rootScope', function( $rootScope ) {
+		document.onkeydown = function( e ) {
+			$rootScope.$apply( function() {
+				$rootScope.$broadcast( 'keydown', e );
+			});
+		}
+	}]);
+}());
